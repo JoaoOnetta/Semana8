cdb4fc56211eb5deacc1f4e99714d50d6b1ceb20
JoaoOnetta <107552614+JoaoOnetta@users.noreply.github.com>
2024-08-09 08:55:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,105 @@
+export const perguntas = [
+    {
+     enunciado: "Qual desses não é um exemplo de IA?",
+     alternativas: [
+         {
+             texto:"Assistente Virtual",
+             afirmacao: [
+                "isso aí! sua resposta esta correta. A IA é uma assistente virtual utilizada para auxiliar os humanos! ",
+                "tarefas repetitivas podem ser muito cansativas"
+             ]
+         },
+         {
+             texto:"Rede Social",
+             afirmacao:
+             ["Sua resposta está errada! a rede social não é uma inteligencia artificial pois não responde perguntas e não resolve problemas!",
+                 "rede social não é um exemplo, pois é apenas um meio de comunicação e não responde perguntas"
+             ]
+         },
+        
+     ]
+    },
+   
+    {
+     enunciado: "Qual é o papel da IA na automação de processos?",
+     alternativas: [
+         {
+             texto:"Substituir completamente os humanos",
+             afirmacao:[
+                 "Sua resposta está errada! Ela somente auxilia os humanos!",
+                 "A IA também depende dos humanos para receber atualizações"
+ 
+             ]
+         },
+         {
+             texto:"Auxiliar os humanos em tarefas repetitivas",
+             afirmacao:[
+                 "Sua resposta está correta! A IA auxilia os humanos nas atividades diárias e repetitivas",
+                 " Tirar duvidas é um bom exempo de sua utilidade!"
+             ]
+         },
+        
+     ]
+    },
+    {
+     enunciado: "O que significa a sigla IA?",
+     alternativas: [
+         {
+             texto:"Instituto Ambiental",
+             afirmacao:[
+                 "errado! São apenas parecidas",
+                 "A sigla refere-se ao instituto ambiental é OIA."
+             ]
+         },
+         {
+             texto:"Inteligência Artificial",
+             afirmacao:[
+                 "Corrreto! A sigla IA é a abreviação de Inteligencia artificial!",
+                 "Esse recurso teve grande revolução nos ultimos anos"
+             ]
+         },
+        
+     ]
+    },
+    {
+     enunciado: "A ética na IA trata de:",
+     alternativas: [
+         {
+             texto:"Garantir que a IA seja usada de forma responsável e ética.",
+             afirmacao:[
+                 "Exato! A IA sempre se preocupa cpm sua ética!",
+                 "Um dos maiores desafios é garantir que os algoritmos sejam justos e não perpetuem preconceitos ou discriminações."
+ 
+             ]
+         },
+         {
+             texto:"Desenvolver IA sem considerar suas implicações éticas.",
+             afirmacao:[
+                 "Não! A IA se importa bastante com sua ética!",
+                 "Caso haja o descumprimento há punições!"
+             ]
+         },
+         
+     ]
+    },
+    {
+     enunciado: "IA fraca se refere a:",
+     alternativas: [
+         {
+             texto: "IA projetada para uma tarefa específica.",
+             afirmacao:[
+                 "certo! A IA fraca é algo projetado para realizar tarefas repetitivas!",
+                 "A IA pode automatizar a montagem e inspeção, identificar defeitos nos produtos"
+             ]
+         },
+         {
+             texto:"IA para multiplas tarefas.",
+             afirmacao:[
+                 "Errado! Esse é um exemplo de IA forte! ",
+                 " Ela tem o propósito de criar máquinas inteligentes que não se distinguem da mente humana."
+             ]
+         },
+         
+     ]
+    }, 
+ ];
